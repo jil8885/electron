2f8b2f2ce2e351e42a77aa00cea8635412cf71bb
jeongin <jil8885@hanyang.ac.kr>
2018-11-27 14:15:48 +0900

    update tuesday

--- renderer.js
+++ renderer.js
@@ -1,19 +1,41 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
-var r101=document.getElementById('r101');
-var confirm=confirm('예약이 들어왔습니다');
-if(confirm==true){
-    alert('확인');
-    r101.addEventListener('click',function() {
-        if(r101.style.background=='white'){
-            r101.style.background =='red';
-        }else{
-            r101.style.background='white';
+
+let rooms = document.querySelectorAll('.room td');
+
+
+
+let mariadb = require('mysql');
+let connection = mariadb.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'a981212',
+    database: 'hotel'
+});
+
+connection.connect();
+
+while(1) {
+    connection.query('select state_transaction from room_information', [], function (err, rows) {
+        if (err) {
+            console.log(err);
         }
-    }
-}else{
-    alert('취소');
+
+        for (let i = 0; i < rows.length; i++) {
+            if (rows[i].state_transaction === 0) {
+                rooms[i].style.backgroundColor = 'white';
+            } else if (rows[i].state_transaction === 1) {
+                rooms[i].style.backgroundColor = 'blue';
+            } else if (rows[i].state_transaction === 2) {
+                rooms[i].style.backgroundColor = 'red';
+            }
+        }
+    });
+    sleep(1000);
 }
 
-
+function sleep (delay) {
+    var start = new Date().getTime();
+    while (new Date().getTime() < start + delay);
+}
